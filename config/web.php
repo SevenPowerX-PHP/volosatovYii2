00a0ff149678276cad0c921cc6c0b5532a6ab018
Acer_splaa <splaandrey@gmail.com>
2018-11-12 03:38:19 +0100

    Added yii2/debug and config/web cookieValidationKey

--- config/web.php
+++ config/web.php
@@ -1,16 +1,26 @@
 <?php
-    return [
-    'id' => 'school',
-	    'basePath' => realpath(__DIR__ . '/../'),
-	    'aliases' => [
-		    '@bower' => '@vendor/bower-asset',
-		    '@npm'   => '@vendor/npm-asset',
-	    ],
-	    'components' =>[
-	    	'urlManager' => [
-			    'enablePrettyUrl' => true,
-			    'showScriptName' => false
-		    ]
-	    ]
-    ];
+	return [
+		'bootstrap' => ['debug'],
+		'id' => 'school',
+		'basePath' => realpath(__DIR__ . '/../'),
+		'aliases' => [
+			'@bower' => '@vendor/bower-asset',
+			'@npm' => '@vendor/npm-asset',
+		],
+		'components' => [
+			'urlManager' => [
+				'enablePrettyUrl' => true,
+				'showScriptName' => false
+			],
+			'request' => [
+				// !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+				'cookieValidationKey' => 'SevenPowerX2018 SecretCode',
+			],
+		],
+		'modules' => [
+			'debug' => [
+				'class' => 'yii\debug\Module',
+			],
+			],
+	];
 ?>
