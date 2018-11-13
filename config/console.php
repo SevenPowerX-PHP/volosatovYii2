5fad0eb7826f65821b1de6b67df39779d66ae052
Acer_splaa <splaandrey@gmail.com>
2018-11-13 20:43:14 +0100

    Added migration for user table

--- config/console.php
+++ config/console.php
@@ -1,5 +1,14 @@
 <?php
 	return [
 		'id' => 'school-console',
-		'basePath' => dirname(__DIR__)
+		'basePath' => dirname(__DIR__),
+		'components' =>[
+			'db' => [
+				'class' => 'yii\db\Connection',
+				'dsn' => 'mysql:host=localhost; dbname=school_loc_win7',
+				'username' => 'root',
+				'password' => '',
+				'charset' => 'utf8'
+			]
+		]
 	];
