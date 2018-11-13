eb5151adf958758a1734fbbb0eba60a90d453026
Acer_splaa <splaandrey@gmail.com>
2018-11-13 19:29:14 +0100

    update user join test Object test

--- tests/_support/Step/Acceptance/TestUserJoin.php
+++ tests/_support/Step/Acceptance/TestUserJoin.php
@@ -6,34 +6,54 @@ class TestUserJoin extends \AcceptanceTester
 
     public function imaginUser()
     {
-        $I = $this;
-	    $user = [];
+
+	    $user = [
+		    'name' => 'John',
+		    'email' => 'splaa@mail.ru',
+		    'password' => 'qwas'
+	    ];
+	    print_r($user);
 	    return $user;
     }
 
     public function joinUser($user)
     {
         $I = $this;
+	    $I->amOnPage('/user/join');
+	    $I->see('Join us');
+	    $I->fillField('UserJoinForm[name]');
+	    $I->fillField('UserJoinForm[email]');
+	    $I->fillField('UserJoinForm[password]');
+	    $I->fillField('UserJoinForm[password2]');
+	    $I->click('Create');
     }
 
     public function loginUser($user)
     {
-        $I = $this;
+	    $I = $this;
+	    $I->amOnPage('/user/login');
+	    $I->see('Lig in');
+	    $I->fillField('UserLoginForm[email]');
+	    $I->fillField('UserLoginForm[password]');
+	    $I->click('Enter');
     }
 
     public function logoutUser()
     {
         $I = $this;
+	    $I->click('Logout');
     }
 
     public function isUserLogged($user)
     {
         $I = $this;
+        $I->see($user['name']);
     }
 
     public function noUserLogged($user)
     {
         $I = $this;
+	    $I->dontSee($user['name']);
     }
 
 }
