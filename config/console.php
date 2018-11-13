be42a42f8b7bd27ee5456f6539f8e4c503499a4d
Acer_splaa <splaandrey@gmail.com>
2018-11-13 21:10:34 +0100

    Refactoring components db to file

--- config/console.php
+++ config/console.php
@@ -3,12 +3,6 @@
 		'id' => 'school-console',
 		'basePath' => dirname(__DIR__),
 		'components' =>[
-			'db' => [
-				'class' => 'yii\db\Connection',
-				'dsn' => 'mysql:host=localhost; dbname=school_loc_win7',
-				'username' => 'root',
-				'password' => '',
-				'charset' => 'utf8'
-			]
+			'db' => require __DIR__ . '/db.php'
 		]
 	];
---
+++ config/db.php
@@ -0,0 +1,8 @@
+<?php
+	return [
+		'class' => 'yii\db\Connection',
+		'dsn' => 'mysql:host=localhost; dbname=school_loc_win7',
+		'username' => 'root',
+		'password' => '',
+		'charset' => 'utf8'
+	];
